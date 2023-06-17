19c2d3d7cccaa80c5298b9b1aa9685145a1f2cb1
Kami1l <kamil.kozik44@gmail.com>
2023-06-17 12:37:24 +0200

    Create 'Config' dictionary for player config

--- main.py
+++ main.py
@@ -7,20 +7,23 @@ SCREEN_HEIGHT = 800
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player_x = 0
-player_y = 0
-player_parts = 3
-player_width = 25
-player_height = 25
-player_move_speed = 0.15
-actualyMove = 0
+Config = {
+    "player_x":0,
+    "player_y":0,
+    "player_width":25,
+    "player_height":25,
+    "player_parts":3,
+    "player_move_speed":0.15,
+    "actualyMove":0
+}
 
 run = True
 
 class Player:
 
     def __init__(self):
-        pass
+        self.rect = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+
     def update(self):
         pass
 
@@ -28,30 +31,30 @@ player = Player()
 
 while run:
     screen.fill((0,0,0))
-    pygame.draw.rect(screen,(255,255,255),(player_x,player_y,player_width,player_height))
+    pygame.draw.rect(screen,(255,255,255),(Config["player_x"],Config["player_y"],Config["player_width"],Config["player_height"]))
     key = pygame.key.get_pressed()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w and actualyMove != "Down":
-                actualyMove = "Top"
-            elif event.key == pygame.K_s and actualyMove != "Top":
-                actualyMove = "Down"
-            elif event.key == pygame.K_a and actualyMove != "Right":
-                actualyMove = "Left"
-            elif event.key == pygame.K_d and actualyMove != "Left":
-                actualyMove = "Right"
+            if event.key == pygame.K_w and Config["actualyMove"] != "Down":
+                Config["actualyMove"] = "Top"
+            elif event.key == pygame.K_s and Config["actualyMove"] != "Top":
+                Config["actualyMove"] = "Down"
+            elif event.key == pygame.K_a and Config["actualyMove"] != "Right":
+                Config["actualyMove"] = "Left"
+            elif event.key == pygame.K_d and Config["actualyMove"] != "Left":
+                Config["actualyMove"] = "Right"
 
-    if actualyMove == "Left":
-        player_x -= player_move_speed
-    elif actualyMove == "Right":
-        player_x += player_move_speed
-    elif actualyMove == "Top":
-        player_y -= player_move_speed
-    elif actualyMove == "Down":
-        player_y += player_move_speed
+    if Config["actualyMove"] == "Left":
+        Config["player_x"]-= Config["player_move_speed"]
+    elif Config["actualyMove"] == "Right":
+        Config["player_x"] += Config["player_move_speed"]
+    elif Config["actualyMove"] == "Top":
+        Config["player_y"] -= Config["player_move_speed"]
+    elif Config["actualyMove"] == "Down":
+        Config["player_y"] += Config["player_move_speed"]
 
 
     pygame.display.update()
