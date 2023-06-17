d999a375695bc5a36920639c9ab79bffa57f3746
Kami1l <kamil.kozik44@gmail.com>
2023-06-17 13:37:00 +0200

    Added food,player,colors dictionary , added randomFoodLocation function ,added collision bettwen food and player

--- main.py
+++ main.py
@@ -1,62 +1,103 @@
 import pygame
+import random
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+Colors = {
+    "black":(0,0,0),
+    "white":(255,255,255),
+    "red":(255,0,0)
+}
 
 Config = {
+    "SCREEN_WIDTH":800,
+    "SCREEN_HEIGHT":800,
+    "BACKGROUND_COLOR":Colors["black"],
+}
+
+Player = {
     "player_x":0,
     "player_y":0,
     "player_width":25,
     "player_height":25,
     "player_parts":3,
-    "player_move_speed":0.15,
+    "player_move_speed":15,
     "actualyMove":0
 }
 
-run = True
+Food = {
+    "food_x":100,
+    "food_y":100,
+    "food_width":10,
+    "food_height":10,
+    "food_color":Colors["red"],
+    "food_amount":0
+}
 
-class Player:
+screen = pygame.display.set_mode((Config["SCREEN_WIDTH"], Config["SCREEN_HEIGHT"]))
+clock = pygame.time.Clock()
 
-    def __init__(self):
-        self.rect = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
-
-    def update(self):
-        pass
-
-player = Player()
-
-while run:
-    screen.fill((0,0,0))
-    pygame.draw.rect(screen,(255,255,255),(Config["player_x"],Config["player_y"],Config["player_width"],Config["player_height"]))
-    key = pygame.key.get_pressed()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w and Config["actualyMove"] != "Down":
-                Config["actualyMove"] = "Top"
-            elif event.key == pygame.K_s and Config["actualyMove"] != "Top":
-                Config["actualyMove"] = "Down"
-            elif event.key == pygame.K_a and Config["actualyMove"] != "Right":
-                Config["actualyMove"] = "Left"
-            elif event.key == pygame.K_d and Config["actualyMove"] != "Left":
-                Config["actualyMove"] = "Right"
-
-    if Config["actualyMove"] == "Left":
-        Config["player_x"]-= Config["player_move_speed"]
-    elif Config["actualyMove"] == "Right":
-        Config["player_x"] += Config["player_move_speed"]
-    elif Config["actualyMove"] == "Top":
-        Config["player_y"] -= Config["player_move_speed"]
-    elif Config["actualyMove"] == "Down":
-        Config["player_y"] += Config["player_move_speed"]
+def randomFoodLocation():
+    Food["food_x"] = round(random.randrange(20, Config["SCREEN_WIDTH"]-20))
+    Food['food_y'] = round(random.randrange(20, Config["SCREEN_HEIGHT"]-20))
 
 
-    pygame.display.update()
+def main():
+    run = True
 
-pygame.quit()
+    while run:
+        # Draw the player,fill the screen,initialize the key
+
+        screen.fill(Config["BACKGROUND_COLOR"])
+        player = pygame.draw.rect(screen,Colors["white"],(Player["player_x"],Player["player_y"],Player["player_width"],Player["player_height"]))
+        key = pygame.key.get_pressed()
+
+        # Food spawning and eating food
+
+        food = pygame.draw.rect(screen,Food["food_color"],(Food['food_x'],Food['food_y'],Food['food_width'],Food['food_height']))
+        if Food["food_amount"] == 0:
+            randomFoodLocation()
+            Food["food_amount"] += 1
+
+        eating = player.colliderect(food)
+        if eating == True:
+            Food["food_amount"] -=1
+
+        # Player move and quit the game
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w and Player["actualyMove"] != "Down":
+                    Player["actualyMove"] = "Top"
+                elif event.key == pygame.K_s and Player["actualyMove"] != "Top":
+                    Player["actualyMove"] = "Down"
+                elif event.key == pygame.K_a and Player["actualyMove"] != "Right":
+                    Player["actualyMove"] = "Left"
+                elif event.key == pygame.K_d and Player["actualyMove"] != "Left":
+                    Player["actualyMove"] = "Right"
+
+        # Blocking the keyboard movement 
+
+        if Player["actualyMove"] == "Left":
+            Player["player_x"]-= Player["player_move_speed"]
+        elif Player["actualyMove"] == "Right":
+            Player["player_x"] += Player["player_move_speed"]
+        elif Player["actualyMove"] == "Top":
+            Player["player_y"] -= Player["player_move_speed"]
+        elif Player["actualyMove"] == "Down":
+            Player["player_y"] += Player["player_move_speed"]
+
+        # Lose condition
+
+        if Player["player_x"] < 0 or Player["player_x"] > Config["SCREEN_WIDTH"] or Player["player_y"] < 0 or Player["player_y"] > Config["SCREEN_HEIGHT"]:
+            break
+
+        clock.tick(20)
+        pygame.display.update()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
